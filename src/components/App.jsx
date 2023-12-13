98e00633fb3eff32acce5973a540015a42148253
Valentyna Dumbrava <valyamoldovan55555@outlook.com>
2023-12-13 15:13:25 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,44 +1,44 @@
-import React, {  useEffect } from 'react';
-import { nanoid } from 'nanoid';
+// import React, {  useEffect } from 'react';
+// import { nanoid } from 'nanoid';
 import ContactForm from './ContactForm/ContactForm';
 import Filter from './Filter/Filter';
 import ContactList from './ContactList/ContactList';
 import css from './App.module.css';
-import { useState } from 'react';
+// import { useState } from 'react';
 
 export default function App() {
-  const [contacts, setContacts] = useState(() => {
-    const data = localStorage.getItem('contactsKey');
-    const contactsParse = JSON.parse(data);
-    if (contactsParse) {
-     return (contactsParse);
-    }
-    else {return [] }
-  });
-  const [filter, setFilter] = useState('');
+  // const [contacts, setContacts] = useState(() => {
+  //   const data = localStorage.getItem('contactsKey');
+  //   const contactsParse = JSON.parse(data);
+  //   if (contactsParse) {
+  //    return (contactsParse);
+  //   }
+  //   else {return [] }
+  // });
+  // const [filter, setFilter] = useState('');
 
-  useEffect(() => {
-    const data = localStorage.getItem('contactsKey');
-    const contactsParse = JSON.parse(data);
-    if (contactsParse) {
-      setContacts([...contactsParse]);
-    }
-  }, []);
+  // useEffect(() => {
+  //   const data = localStorage.getItem('contactsKey');
+  //   const contactsParse = JSON.parse(data);
+  //   if (contactsParse) {
+  //     setContacts([...contactsParse]);
+  //   }
+  // }, []);
 
-  useEffect(() => {
-    localStorage.setItem('contactsKey', JSON.stringify(contacts));
-  }, [contacts]);
+  // useEffect(() => {
+  //   localStorage.setItem('contactsKey', JSON.stringify(contacts));
+  // }, [contacts]);
 
-  const handleAddContact = ({ name, number }) => {
-    const newContact = {
-      name: name,
-      number: number,
-      id: `id-` + nanoid(),
-    };
-    return contacts.find(contact => contact.name === newContact.name)
-      ? alert(`${newContact.name} is already in contact`)
-      : setContacts([newContact, ...contacts]);
-  };
+  // const handleAddContact = ({ name, number }) => {
+  //   const newContact = {
+  //     name: name,
+  //     number: number,
+  //     id: `id-` + nanoid(),
+  //   };
+  //   return contacts.find(contact => contact.name === newContact.name)
+  //     ? alert(`${newContact.name} is already in contact`)
+  //     : setContacts([newContact, ...contacts]);
+  // };
 
  
 
